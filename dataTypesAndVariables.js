5aaa1d5604b3e1758d52797c3ae70738024b4c87
Oksana Koretska <koretskaoksana96@gmail.com>
2021-11-02 16:58:03 +0200

    data types and variables

---
+++ dataTypesAndVariables.js
@@ -0,0 +1,144 @@
+// console.log('Hello world');
+// console.log(9+'8');
+// const surname = 'Koretska';
+// let age = 25;
+// let bol = true;
+// console.log( typeof age, typeof surname, typeof bol);
+
+// function sayHello(){
+//     let message = 'Hello ' + surname;
+//     console.log(message);
+// }
+// sayHello();
+
+// function simpleMath(a, b){
+//     let res = a + b;
+//     return res;
+// }
+
+// let sum = simpleMath(11, 22);
+// console.log(sum);
+// console.log('Oksana'.toUpperCase());
+
+// let n = 6;
+// console.log(typeof n);
+
+// var N = new Number(4000),
+//     n=7;
+// console.log(typeof N);
+// console.log(N.toFixed(2));
+// console.log(n.toFixed(4));
+// console.log(2 .toFixed(2));
+
+// let i = 10;
+// console.log(i);
+// console.log(++i);
+// console.log(i++);
+// console.log(i);
+
+// console.log(10 > 5);
+
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(5, 6));
+// console.log(Math.PI);
+// console.log(Math.E);
+
+// console.log(typeof Infinity);
+
+// console.log('Oksana'.length);
+
+// var str = 'I love you to the moon and back';
+// console.log(str.length);
+// console.log(str.charAt(str.length - 1));
+
+// console.log(str.substring(2));
+// console.log(str.substring(2, 6));
+// console.log(str.slice(-4));
+// console.log(str.slice(4));
+// console.log(str.substr(2, 4));
+
+// console.log(str.indexOf('l'));
+// console.log(str.replace('and back', ''))
+// console.log(str.split(" "));
+// console.log(str[3]);
+
+// console.log(Number('693'));
+// console.log(typeof(Number('693')));
+
+// console.log(String(569));
+// console.log(typeof(String(569)));
+
+// var num = 56;
+// console.log(num.toString());
+// console.log(typeof num.toString());
+
+// console.log(parseInt('45'));
+// console.log(typeof parseInt('45'));
+
+// console.log(parseInt('45.35'));
+// console.log(typeof parseInt('45.35'));
+
+// console.log(parseFloat('45.35'));
+// console.log(typeof parseFloat('45.35'));
+
+
+// if(true) console.log('Its true');
+
+// let n = 5;
+// if (n > 3){
+// n *= 3;
+// console.log(n);
+// }
+
+
+
+
+// if(usName === 'Oksana'){
+//     homecity = 'Lviv';
+// } else if(usName === 'Lily'){
+//     homecity = 'Kyiv';
+// } else (usName){
+//     homecity = 'other';
+// }
+    
+// console.log(homecity);
+
+// let userName = 'Oleh', homecity;
+
+// switch(userName){
+//     case "Oksana": homecity = "Lviv"; break
+//     case "Lily": homecity = "Kyiv"; break
+//     case "Olga": homecity = "LA";
+//     default : homecity = "World";
+
+// }
+// console.log(homecity);
+console.log("Hello Oksana")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
