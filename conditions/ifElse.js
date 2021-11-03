75bc69aef4105755d4163ad5a69117f32798c004
Oksana Koretska <koretskaoksana96@gmail.com>
2021-11-03 13:43:06 +0200

    conditions plus updated data types

---
+++ conditions/ifElse.js
@@ -0,0 +1,28 @@
+// let a = 'Oksana';
+// let b = 7;
+// let c = -10;
+
+// if (a === 5){
+//     console.log('a is equal to 5');
+// } else if (a > 5){
+//     console.log('a is equal to another digits');
+// } else{
+//     console.log('Some string');
+// }
+
+
+// if (c > 1) {
+//     console.log("c is bigger than 1");
+//  } else if (c == 10) {
+//     console.log("c is equal 10");
+//  } else {
+//     console.log("c is smaller than 1");
+//  }
+
+
+//  if (b > 1) {
+//     console.log("b is bigger than 1");
+//  } else {
+//     console.log("b is smaller than 1");
+//  }
+
---
+++ conditions/switch.js
@@ -0,0 +1,24 @@
+const car = "Mazda";
+
+switch (car) {
+  case "BMW": {
+      console.log("Germany");
+  } 
+  break;
+    
+  case "Peugeot": {
+      console.log("France");
+  } 
+  break;
+
+  case "Jeep": {
+      console.log("USA")
+  }
+  break;
+    
+  default: {
+      console.log("Some another country");
+  } 
+}
+
+
